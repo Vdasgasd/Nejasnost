540bc1f086f8604b96d22a71dd94076a44b0544d
Sandiaz <94012964+Vdasgasd@users.noreply.github.com>
2024-08-26 13:55:45 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/site.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite: ////tmp/site.db'
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
